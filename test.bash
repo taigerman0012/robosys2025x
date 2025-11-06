c89e802c6411086be4b5af2b7680b3167d5da6e2
Taiga Doi <s24C1087re@s.chibakoudai.jp>
2025-11-06 13:23:27 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,13 +1,16 @@
-#!/bin/bash
-
-ng() {
+#!/bin/bash -xv
+# 著作権
+ng () {
 	echo ${1}行目が違うよ #$1はngの1番目の引数
 	res=1
 }
 
 res=0 
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"  
-[ "$a" = 山田 ] || ng "$LINENO"
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+
+[ "${res}" = 0 ] && echo OK
 
 exit $res
