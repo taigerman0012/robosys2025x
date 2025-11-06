71f22d583dbd3c9a8ec5f6c807ee2feba13e1fea
Taiga Doi <s24C1087re@s.chibakoudai.jp>
2025-11-06 14:00:29 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -3,16 +3,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ #$1はngの1番目の引数
-	res=1
+        echo ${1}行目が違うよ
+        res=1
 }
 
-res=0 
+res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
 
-[ "${res}" = 0 ] && echo OK
-
+[ "$res" = 0 ] && echo OK
 exit $res
